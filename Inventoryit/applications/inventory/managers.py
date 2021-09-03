079f0c9ec2ea48ebf1a148edb30c9434a9591554
Neldis Barrios <neldis12@hotmail.com>
2021-09-03 13:23:35 +0200

    Update managers.py

--- Inventoryit/applications/inventory/managers.py
+++ Inventoryit/applications/inventory/managers.py
@@ -1,9 +1,7 @@
 from django.db import models
-
 from django.db.models import Q
 
 class InventoryManagers(models.Manager):
-
     def list_all_filter(self,keyword, acronym):
         result= self.filter(
             Q(nuuma__icontains=keyword) | Q(location__icontains=keyword)
